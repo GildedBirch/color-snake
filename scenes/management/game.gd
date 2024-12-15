182a3dec0eb9f90a5e1d986b90b7445d57782258
GildedBirch <thebirchbreaker@gmail.com>
2024-12-15 16:22:21 +0200

    Snake color changes

--- scenes/management/game.gd
+++ scenes/management/game.gd
@@ -16,16 +16,16 @@ const DIR: Dictionary = {
 	LEFT : Vector2i.LEFT,
 	RIGHT : Vector2i.RIGHT
 }
-
 var input_direction: int = LEFT
 var prev_dir: int = LEFT
+
+var score: int = 0
 
 @export var tile_size: int = 16
 @export var map_size: Vector2i = Vector2i(20, 15)
 
 func _ready() -> void:
 	initialize_snake(10)
-	print(map_size)
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_up"):
@@ -38,11 +38,11 @@ func _input(_event: InputEvent) -> void:
 		input_direction = RIGHT
 
 func initialize_snake(length: int) -> void:
-	var picked_color: Color = colors.pick_random()
+	var color = colors.pick_random()
 	for i in range(length):
 		var snake_part: SnakePart = SNAKE_PART.instantiate()
 		%Snake.add_child(snake_part)
-		snake_part.color = picked_color
+		snake_part.color = color
 		snake_part.tile_position = Vector2i(floori(map_size.x/2) + i, floori(map_size.y/2))
 		snake_part.position = Vector2i(
 			floori(map_size.x/2)*tile_size + i*tile_size,
@@ -54,16 +54,43 @@ func move(dir: int):
 	prev_dir = dir
 	
 	var prev_pos: Vector2i = %Snake.get_child(0).tile_position + DIR[dir]
-	eat(prev_pos)
+	if eat(prev_pos):
+		return
+	
+	var prev_color: Color = %Snake.get_child(0).color
 	
 	for snake_part: SnakePart in %Snake.get_children():
 		var new_pos: Vector2i = prev_pos
 		prev_pos = snake_part.tile_position
 		snake_part.tile_position = new_pos
+		var new_color: Color = prev_color
+		prev_color = snake_part.color
+		snake_part.color = new_color
 		snake_part.position = tile_to_world_pos(snake_part.tile_position)
 
-func eat(prev_pos: Vector2i):
-	var food: Array = %Food.get_children()
+func eat(prev_pos: Vector2i) -> bool:
+	var food: Dictionary = {}
+	for food_item: Food in %Food.get_children():
+		food[food_item.tile_position] = food_item
+	
+	if food.has(%Snake.get_child(0).tile_position):
+		var food_color = food[%Snake.get_child(0).tile_position].color
+		food[%Snake.get_child(0).tile_position].queue_free()
+		if food_color != %Snake.get_child(0).color:
+			score += 2
+		else:
+			score += 1
+		add_snake_part(prev_pos, food_color)
+		return true
+	return false
+
+func add_snake_part(pos: Vector2i, food_color: Color):
+	var snake_part: SnakePart = SNAKE_PART.instantiate()
+	%Snake.add_child(snake_part)
+	snake_part.color = food_color
+	snake_part.tile_position = pos
+	snake_part.position = tile_to_world_pos(pos)
+	%Snake.move_child(snake_part, 0)
 
 func spawn_food():
 	var pos: Vector2i = Vector2i(randi_range(1, map_size.x-2), randi_range(1, map_size.y-2))
@@ -80,3 +107,4 @@ func _on_game_tick_timer_timeout() -> void:
 	move(input_direction)
 	if randf() > 0.8:
 		spawn_food()
+	%ScoreLabel.text = "Score: %s  |  Length: %s" % [score, %Snake.get_children().size()]
