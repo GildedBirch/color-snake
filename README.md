325481fc93cfc9825760c3a9dab2f434303790a4
GildedBirch <thebirchbreaker@gmail.com>
2024-12-16 10:44:45 +0200

    Added font and more work on UI

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Snake game for Game Jam
 https://itch.io/jam/classics
+
+Font: Monogram by datagoblin https://datagoblin.itch.io/monogram
--- scenes/UI/game_over_screen.gd
+++ scenes/UI/game_over_screen.gd
@@ -3,3 +3,9 @@ extends PanelContainer
 func set_scores(score: int, snake_length: int):
 	%ScoreLabel.text = "Score: %s" % score
 	%LengthLabel.text = "Length %s" % snake_length
+
+func _on_retry_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_menu_button_pressed() -> void:
+	pass # Replace with function body.
